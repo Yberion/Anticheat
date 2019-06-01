9b7e9d5831f0a33eead928b18497be710e3384ea
Brandon <Yberion@users.noreply.github.com>
2019-06-01 04:35:11 +0200

    Set back sv_snapsPolicy to 1

--- codemp/server/sv_init.cpp
+++ codemp/server/sv_init.cpp
@@ -978,7 +978,7 @@ void SV_Init (void) {
 	sv_privatePassword = Cvar_Get ("sv_privatePassword", "", CVAR_TEMP );
 	sv_snapsMin = Cvar_Get ("sv_snapsMin", "0", CVAR_ARCHIVE_ND ); // 1 <=> sv_snapsMax
 	sv_snapsMax = Cvar_Get ("sv_snapsMax", "0", CVAR_ARCHIVE_ND ); // sv_snapsMin <=> sv_fps
-	sv_snapsPolicy = Cvar_Get ("sv_snapsPolicy", "2", CVAR_ARCHIVE_ND, "Determines which policy of enforcement is used for client's \"snaps\" cvar");
+	sv_snapsPolicy = Cvar_Get ("sv_snapsPolicy", "1", CVAR_ARCHIVE_ND, "Determines which policy of enforcement is used for client's \"snaps\" cvar");
 	Cvar_CheckRange(sv_snapsPolicy, 0, 2, qtrue);
 	sv_fps = Cvar_Get ("sv_fps", "20", CVAR_SERVERINFO, "Server frames per second" );
 	Cvar_CheckRange(sv_fps, 0, 1000, qtrue);
